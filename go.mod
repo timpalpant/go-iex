076cc758e20e38c96f039efdc02f8572fd0cd82e
Joshua Harrison <josh.harrison@gmail.com>
2019-09-30 21:28:19 -0600

    Add a type for dealing with IEX URLs.

--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
 module github.com/timpalpant/go-iex
 
 require (
+	github.com/chilts/sid v0.0.0-20190607042430-660e94789ec9
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/google/go-cmp v0.2.0 // indirect
 	github.com/google/go-querystring v0.0.0-20170111101155-53e6ce116135
 	github.com/google/gopacket v1.1.16-0.20181023151400-a35e09f9f224
+	github.com/gorilla/websocket v1.4.1
 	github.com/johnmccabe/go-bitbar v0.4.0
 	github.com/mdlayher/raw v0.0.0-20181016155347-fa5ef3332ca9 // indirect
 	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337
---
+++ socketio/endpoints_test.go
@@ -0,0 +1,102 @@
+package socketio_test
+
+import (
+	"net/url"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+	. "github.com/timpalpant/go-iex/socketio"
+)
+
+func TestIEXEndpoint(t *testing.T) {
+	Convey("The Endpoint", t, func() {
+		Convey("should have the correct base http URL", func() {
+			endpoint := NewIEXEndpoint(func() string {
+				return "123"
+			})
+			to, err := url.Parse(endpoint.GetHTTPUrl())
+			So(err, ShouldBeNil)
+			So(to.Scheme, ShouldEqual, "https")
+			So(to.Host, ShouldEqual, "ws-api.iextrading.com")
+			So(to.Path, ShouldEqual, "/socketio/")
+			values := to.Query()
+			So(values.Get("EIO"), ShouldEqual, "3")
+			So(values.Get("transport"), ShouldEqual, "polling")
+		})
+		Convey("should have the correct base Websocket URL", func() {
+			endpoint := NewIEXEndpoint(func() string {
+				return "123"
+			})
+			to, err := url.Parse(endpoint.GetWSUrl())
+			So(err, ShouldBeNil)
+			So(to.Scheme, ShouldEqual, "wss")
+			So(to.Host, ShouldEqual, "ws-api.iextrading.com")
+			So(to.Path, ShouldEqual, "/socketio/")
+			values := to.Query()
+			So(values.Get("EIO"), ShouldEqual, "3")
+			So(values.Get("transport"), ShouldEqual, "websocket")
+		})
+		Convey("should set the SID on the HTTP URL", func() {
+			sid := "4567"
+			endpoint := NewIEXEndpoint(func() string {
+				return "123"
+			})
+			endpoint.SetSid(sid)
+			to, err := url.Parse(endpoint.GetHTTPUrl())
+			So(err, ShouldBeNil)
+			values := to.Query()
+			So(values.Get("sid"), ShouldEqual, sid)
+		})
+		Convey("should set the SID on the Websocket URL", func() {
+			sid := "4567"
+			endpoint := NewIEXEndpoint(func() string {
+				return "123"
+			})
+			endpoint.SetSid(sid)
+			to, err := url.Parse(endpoint.GetWSUrl())
+			So(err, ShouldBeNil)
+			values := to.Query()
+			So(values.Get("sid"), ShouldEqual, sid)
+		})
+		Convey("should change 't' for HTTP URLs", func() {
+			timestamps := []string{"123", "456"}
+			index := 0
+			endpoint := NewIEXEndpoint(func() string {
+				timestamp := timestamps[index]
+				index++
+				return timestamp
+			})
+			to, err := url.Parse(endpoint.GetHTTPUrl())
+			So(err, ShouldBeNil)
+			values := to.Query()
+			first := values.Get("t")
+
+			to, err = url.Parse(endpoint.GetHTTPUrl())
+			So(err, ShouldBeNil)
+			values = to.Query()
+			second := values.Get("t")
+
+			So(first, ShouldNotEqual, second)
+		})
+		Convey("should change 't' for WS URLs", func() {
+			timestamps := []string{"123", "456"}
+			index := 0
+			endpoint := NewIEXEndpoint(func() string {
+				timestamp := timestamps[index]
+				index++
+				return timestamp
+			})
+			to, err := url.Parse(endpoint.GetWSUrl())
+			So(err, ShouldBeNil)
+			values := to.Query()
+			first := values.Get("t")
+
+			to, err = url.Parse(endpoint.GetWSUrl())
+			So(err, ShouldBeNil)
+			values = to.Query()
+			second := values.Get("t")
+
+			So(first, ShouldNotEqual, second)
+		})
+	})
+}
